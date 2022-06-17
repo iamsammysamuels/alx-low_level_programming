01abdb7ec5ab467febad355dad678cebb128b2d0
iamsammysamuels <iamsammysamuels@gmail.com>
2022-06-16 21:27:04 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,28 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * cap_string - capitalizes all words of a string.
- * @str: the string to change the first letter of a word in uppercase.
- *
+ * cap_string - capitalizes all words of a string
+ * @str: the string to change the first letter of a word in uppercase
  * Return: capitalizes letters
  */
+
 char *cap_string(char *str)
 {
-int l, m;
-char k[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+int i, j;
+char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 i = 0;
 while (str[i] != '\0')
 {
-if (i == 0 && str[i] >= 97 && str[i] <= 122)
+	if (i == 0 && str[i] >= 97 && str[i] <= 122)
 {
-str[i] = str[i] - 32;
+	str[i] = str[i] - 32;
 }
 j = 0;
-while (c[j] != '0')
+while (c[j] != '\0')
 {
-if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+	if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
 {
-str[i + 1] = str[i + 1] - 32;
+	str[i + 1] = str[i + 1] - 32;
 }
 j++;
 }
