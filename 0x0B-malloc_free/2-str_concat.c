0ae8168d3ffdbbe9a0c346687d51bdfeb0857fca
iamsammysamuels <101473397+iamsammysamuels@users.noreply.github.com>
2022-06-29 03:10:14 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,7 +8,6 @@
  * @s2: An input pointer of the second string
  * Return: Apointer to concatened strings or NULL if it str is NULL
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *new_str, *starts1, *starts2;
@@ -24,32 +23,31 @@ char *str_concat(char *s1, char *s2)
 		s1++;
 	}
 	s1 = starts1;
-
-		if (s2 == NULL)
-			s2 = "";
-		while (*s2)
+	if (s2 == NULL)
+		s2 = "";
+	while (*s2)
+	{
+		lens2++;
+		s2++;
+	}
+	s2 = starts2;
+	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	starts1 = new_str;
+	if (new_str == NULL)
+		return (NULL);
+	for (; i < (lens1 + lens2); i++)
+	{
+		if (i < lens1)
 		{
-			lens2++;
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
 			s2++;
 		}
-		s2 = starts2;
-		new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
-		starts1 = new_str;
-		if (new_str == NULL)
-			return (NULL);
-		for (; i < (lens1 + lens2); i++)
-		{
-			if (i < lens1)
-			{
-				new_str[i] = *s1;
-				s1++;
-			}
-			else
-			{
-				new_str[i] = *s2;
-				s1++;
-			}
-		}
-		new_str[i] = '\0';
-		return (starts1);
+	}
+	new_str[i] = '\0';
+	return (starts1);
 }
