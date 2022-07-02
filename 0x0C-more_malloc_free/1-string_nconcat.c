4328af9bc3dad355b1de3626a2aefa4015b8ec82
iamsammysamuels <iamsammysamuels@gmail.com>
2022-07-01 22:46:59 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *nw_str;
 	unsigned int i = 0;
-
-	lens1 = 0;
-	lens2 = 0;
+	unsigned int lens1 = 0;
+	unsigned int lens2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
