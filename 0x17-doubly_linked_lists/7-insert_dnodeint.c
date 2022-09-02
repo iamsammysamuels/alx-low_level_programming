7fe459d4412e2d74f3db050041f688fcdab5d07a
iamsammysamuels <iamsammysamuels@gmail.com>
2022-09-01 20:05:17 -0700

    Linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,43 +2,51 @@
 #include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - insert node at specific index
- * @h: head of linked list
- * @idx: index of new node
- * @n: new node value
- * Return: inserted node
+ * insert_dnodeint_at_index - A function that inserts a node at
+ * position in a list.
+ * @h: The double pointer to the head.
+ * @idx: The index to insert new node at.
+ * @n: The data to add to new node.
+ * Return: A pointer to new element, or NULL on failure.
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new;
+	dlistint_t *new_node = NULL, *temp = NULL;
+	unsigned int i = 0;
 
-	if (h == NULL)
-		return (0);
-
-	current = *h;
-
-	while (idx != 0)
-	{
-		current = current->next;
-		idx--;
-		if (current == NULL)
-			return (NULL);
-	}
-
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-	{
-		free(new);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
 		return (NULL);
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+	if (!h || !(*h))
+		*h = new_node;
+	else
+	{
+		temp = *h;
+		while (idx != i++ && temp->next)
+			temp = temp->next;
+		if (temp->next)
+			new_node->prev = temp->prev;
+		else
+			new_node->prev = temp;
+		if (idx == i)
+			temp->next = new_node, new_node->prev = temp;
+		else if (idx == i - 1)
+		{
+			if (temp->prev)
+				temp->prev->next = new_node;
+			else
+				*h = new_node;
+			temp->prev = new_node;
+			new_node->next = temp;
+		}
+		else
+		{
+			free(new_node);
+			return (NULL);
+		}
 	}
-
-	new->n = n;
-	new->next = current;
-	new->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = new;
-	return (current);
+	return (new_node);
 }
